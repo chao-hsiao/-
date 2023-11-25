6588a28459ff87f1f4e4544b019eec1c7d17f755
chao-hsiao <79911183+chao-hsiao@users.noreply.github.com>
2023-11-26 07:38:38 +0800

    Update setup.sh

--- docker-compose/setup.sh
+++ docker-compose/setup.sh
@@ -25,10 +25,8 @@ sudo chmod +x /usr/local/bin/docker-compose
 # Start the Docker service
 sudo systemctl start docker
 
-# Navigate to the docker-compose directory and start the web server
+# Start the web server
 sudo docker-compose up -d
 
 # Open MySQL server terminal
-# Replace 'docker-compose_db_1' with your actual Docker MySQL container name
-# Replace 'mydatabase' with your actual database name
 sudo docker exec -it docker-compose_db_1 mysql --local_infile=1 -u root -p mydatabase
