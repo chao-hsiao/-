09c3df0bc5663daa3dfaaea4ea50e2beca0a966b
chao-hsiao <79911183+chao-hsiao@users.noreply.github.com>
2023-11-26 07:21:39 +0800

    Update setup.sh

--- docker-compose/setup.sh
+++ docker-compose/setup.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-# Update and upgrade the system
-sudo apt-get update && sudo apt-get upgrade -y
+# Upgrade the system
+sudo apt-get upgrade -y
 
 # Install Docker
-sudo apt-get install docker.io -y
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+
 
 # Start the Docker service
 sudo systemctl start docker
