d25523142a1ebab6488885a26d9174ad2c3dda3f
chao-hsiao <79911183+chao-hsiao@users.noreply.github.com>
2023-11-26 06:31:53 +0800

    Add files via upload

---
+++ docker-compose/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Update and upgrade the system
+sudo apt-get update && sudo apt-get upgrade -y
+
+# Install Docker
+sudo apt-get install docker.io -y
+
+# Start the Docker service
+sudo systemctl start docker
+
+# Navigate to the docker-compose directory and start the web server
+cd /path/to/project/docker-compose  # Replace with your actual docker-compose directory path
+sudo docker-compose up -d
+
+# Open MySQL server terminal
+# Replace 'docker-compose_db_1' with your actual Docker MySQL container name
+# Replace 'mydatabase' with your actual database name
+sudo docker exec -it docker-compose_db_1 mysql --local_infile=1 -u root -p mydatabase
