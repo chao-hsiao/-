0640335cbd6fddbc0edf291712eeff624f9af1c5
chao-hsiao <79911183+chao-hsiao@users.noreply.github.com>
2023-11-26 07:32:05 +0800

    Update setup.sh

--- docker-compose/setup.sh
+++ docker-compose/setup.sh
@@ -18,13 +18,15 @@ sudo add-apt-repository \
 sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 
 # Start the Docker service
 sudo systemctl start docker
 
 # Navigate to the docker-compose directory and start the web server
-docker-compose up -d
+sudo docker-compose up -d
 
 # Open MySQL server terminal
 # Replace 'docker-compose_db_1' with your actual Docker MySQL container name
