c48ab7208799de5c089b35407d5120a77790b1f7
chao-hsiao <79911183+chao-hsiao@users.noreply.github.com>
2023-11-26 13:02:02 +0800

    Update queries.sql

--- queries/queries.sql
+++ queries/queries.sql
@@ -1,10 +1,7 @@
-select * from taiwan_cities;
+SET NAMES 'UTF8';
 select * from lvr_land_c;
 select * from lvr_land_c_build;
 select * from lvr_land_c_land;
 select * from lvr_land_c_park;
 select * from taiwan_cities;
 select * from taiwan_districts;
-
-
-SET global_local_infile=true;
