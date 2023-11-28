3be5401124a75d958eb7658d1f38f980a05207e5
chao-hsiao <79911183+chao-hsiao@users.noreply.github.com>
2023-11-28 16:33:17 +0800

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 import mysql.connector
 import json
 import os
@@ -28,20 +28,54 @@ def get_db_connection():
     return connection
 
 # Define a route for the API
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def home():
     connection = get_db_connection()
     if connection:
         cursor = connection.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM taiwan_cities")  # Replace with your actual table name
-        results = cursor.fetchall()
-        cursor.close()
-        connection.close()
-        data=jsonify(results)
-        # return data
-        return render_template('index.html',data=data)
+        if request.method == 'GET':
+            cursor.execute("SELECT * FROM taiwan_cities")
+            cities = cursor.fetchall()
+            cursor.execute("SELECT DISTINCT 出租型態 FROM lvr_land_c")
+            rental_types = cursor.fetchall()
+            cursor.close()
+            connection.close()
+            return render_template('index.html',data={'cities': cities, 'rental_types': rental_types})
+        elif request.method == 'POST':
+            data = request.json
+            selected_city_id = data['selected_city_id']
+            cursor.execute("SELECT * FROM taiwan_districts WHERE city_id = %s", (int(selected_city_id),))
+            results = cursor.fetchall()
+            cursor.close()
+            connection.close()
+            return jsonify(results)
     else:
         return "Failed to connect to the database", 500
+
+@app.route('/results', methods=['POST'])
+def results():
+    results = {
+        'city': request.form.get('city'),
+        'district': request.form.get('district'),
+        'lowerprice': request.form.get('lowerprice'),
+        'upperprice': request.form.get('upperprice'),
+        'lowerarea': request.form.get('lowerarea'),
+        'upperarea': request.form.get('upperarea'),
+        'rental_type': request.form.get('rental_type'),
+        'keyword': request.form.get('keyword')
+    }
+    # return render_template('results.html',results=data)
+    return jsonify(results)
+    # connection = get_db_connection()
+    # if connection:
+    #     cursor = connection.cursor(dictionary=True)
+    #     cursor.execute("SELECT * FROM taiwan_districts WHERE city_id = %s", (int(selected_city_id),))
+    #     results = cursor.fetchall()
+    #     cursor.close()
+    #     connection.close()
+    #     return jsonify(results)
+    # else:
+    #     return "Failed to connect to the database", 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
