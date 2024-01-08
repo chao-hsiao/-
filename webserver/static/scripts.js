d84f3d73e959c802f7ca57eda7babb245d70d892
chao-hsiao <79911183+chao-hsiao@users.noreply.github.com>
2024-01-08 22:03:06 +0800

    Update scripts.js

--- webserver/static/scripts.js
+++ webserver/static/scripts.js
@@ -1,3 +1,24 @@
+function getDistrictOptions(id) {
+    fetch('/getDistrictOptions', 
+    {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json',},
+        body: JSON.stringify({selected_id: id})
+    })
+    .then(response => response.text())
+    .then(data => {
+        var select_options = document.getElementById("select_district");
+        select_options.innerHTML = '<option value="" selected="selected">請選擇</option>';
+        var districts = JSON.parse(data);
+        for (var i = 0; i < districts.length; i++) {
+            var options = document.createElement("OPTION");
+            var txt = document.createTextNode(districts[i]["鄉鎮名"]);
+            options.appendChild(txt);
+            options.setAttribute("value",districts[i]["district_id"]);
+            select_options.add(options);
+        }
+    });
+}
 
 var select_options = document.getElementById("select_city");
 var data = document.getElementById('data').textContent;
@@ -7,70 +28,25 @@ for (var i = 0; i < cities.length; i++) {
     var txt = document.createTextNode(cities[i]["縣市名"]);
     selected_cities.appendChild(txt);
     selected_cities.setAttribute("value",cities[i]["city_id"]);
-    select_options.insertBefore(selected_cities,select_options.lastChild);
+    select_options.add(selected_cities);
 }
-selected_city(document.getElementById('select_city').value);
+getDistrictOptions(document.getElementById('select_city').value);
 
-document.getElementById('select_city').addEventListener('change', function() {selected_city(this.value);});
-
-function selected_city(selectedCityId) {
-    fetch('/', 
-    {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json',},
-        body: JSON.stringify({selected_city_id: selectedCityId})
-    })
-    .then(response => response.text())
-    .then(data => {
-        var select_options = document.getElementById("select_district");
-        select_options.innerHTML = '<option value="0" selected="selected">請選擇</option>';
-        var districts = JSON.parse(data);
-        for (var i = 0; i < districts.length; i++) {
-            var districts_options = document.createElement("OPTION");
-            var txt = document.createTextNode(districts[i]["鄉鎮名"]);
-            districts_options.appendChild(txt);
-            districts_options.setAttribute("value",districts[i]["district_id"]);
-            select_options.insertBefore(districts_options,select_options.lastChild);
-        }
-    });
-}
+document.getElementById('select_city').addEventListener('change', function() {getDistrictOptions(this.value);});
 
 var select_options = document.getElementById("rental_type");
 select_options.innerHTML = '<option value="0" selected="selected">不限</option>';
-var rental_types = JSON.parse(data)['rental_types'];
-for (var i = 0; i < rental_types.length; i++) {
-    var selected_cities = document.createElement("OPTION");
-    if (rental_types[i]["出租型態"]) {
-        var txt = document.createTextNode(rental_types[i]["出租型態"]);
+var rental_type = JSON.parse(data)['rental_type'];
+for (var i = 0; i < rental_type.length; i++) {
+    var options = document.createElement("OPTION");
+    if (rental_type[i]["出租型態"]) {
+        var txt = document.createTextNode(rental_type[i]["出租型態"]);
     } else {
         var txt = document.createTextNode("未分類的");
     }
-    selected_cities.appendChild(txt);
-    select_options.insertBefore(selected_cities,select_options.lastChild);
+    options.appendChild(txt);
+    select_options.add(options);
 }
-
-// document.getElementById('submitBtn').addEventListener('click', function() {submit();});
-// function submit() {
-//     var return_results = {
-//         city: document.getElementById('select_city').value,
-//         district: document.getElementById('select_district').value,
-//         lowerprice: document.getElementById('lowerprice').value,
-//         upperprice: document.getElementById('upperprice').value,
-//         lowerarea: document.getElementById('lowerarea').value,
-//         upperarea: document.getElementById('upperarea').value,
-//         rental_type: document.getElementById('rental_type').value
-//     };
-
-//     fetch('/results', 
-//     {
-//         method: 'POST',
-//         headers: {'Content-Type': 'application/json',},
-//         body: JSON.stringify(return_results)
-//     })
-//     .then(response => response.text())
-//     .then(data => {console.log(data)
-//     });
-// }
 
 function validateNumericInput(id) {
     var input = document.getElementById(id).value;
@@ -83,7 +59,7 @@ function validateNumericInput(id) {
 
 function addFunction() {
     // Logic to handle 'Add'
-    window.location.href = 'add_data';
+    window.location.href = 'add_item';
 }
 
 function deleteFunction() {
